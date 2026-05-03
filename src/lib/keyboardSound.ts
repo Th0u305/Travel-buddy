e192fe5dcfacd2c193f10a82d6d91df38887ff23
Th0u305 <ahnaf4411@gmail.com>
2026-05-04 01:21:17 +0600

    fix Audio is not defined

--- src/lib/keyboardSound.ts
+++ src/lib/keyboardSound.ts
@@ -1,16 +1,19 @@
-const keyStrokeSounds = [
-  new Audio("/sound/keystroke1.mp3"),
-  new Audio("/sound/keystroke2.mp3"),
-  new Audio("/sound/keystroke3.mp3"),
-  new Audio("/sound/keystroke4.mp3"),
-];
+// const keyStrokeSounds = [
+//   new Audio("/sound/keystroke1.mp3"),
+//   new Audio("/sound/keystroke2.mp3"),
+//   new Audio("/sound/keystroke3.mp3"),
+//   new Audio("/sound/keystroke4.mp3"),
+// ];
 
 function useKeyboardSound() {
   const playRandomKeyStrokeSound = () => {
-    const randomSound = keyStrokeSounds[Math.floor(Math.random() * keyStrokeSounds.length)];
+    // const randomSound = keyStrokeSounds[Math.floor(Math.random() * keyStrokeSounds.length)];
+   
+    const randomNum = Math.floor(Math.random() * 4) + 1;
+    const audio = new Audio(`/sound/keystroke${randomNum}.mp3`);
 
-    randomSound.currentTime = 0; // this is for a better UX, def add this
-    randomSound.play().catch((error) => console.log("Audio play failed:", error));
+    // randomSound.currentTime = 0; // this is for a better UX, def add this
+    audio.play().catch((error) => console.log("Audio play failed:", error));
   };
 
   return { playRandomKeyStrokeSound };
